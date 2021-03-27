e80e3e829636fd8bb7c1be3a1d5cf35541bd1cb2
Litl Toast <joshuafhiggins@gmail.com>
2021-03-27 15:20:13 -0400

    Restarting from scratch with proper documentation
    Should have done this from the beginning and not worry about cheating because it's meant for people at my school. Reorganized old assets and they will eventually be moved back after I get core systems in place and finish asset ripping. I'll merge them after that.

---
+++ Assets/Scripts/ClientSetup.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ClientSetup : NetworkBehaviour
+{
+    public List<Behaviour> DisableForNonLocalPlayers;
+    public int remotePlayerLayer = 12;
+    Camera SceneCamera;
+
+    //Called when the NetworkManager Instantiates the player, local or NOT local
+    void Start()
+    {
+        //If we are NOT the local player...
+        if (!isLocalPlayer)
+        {
+            //Disble movement and other things
+            for (int i = 0; i < DisableForNonLocalPlayers.Count; i++)
+            {
+                DisableForNonLocalPlayers[i].enabled = false;
+            }
+
+            //Set the gameobjects in the player to the remotePlayer layer, 12, so they are rendered
+            //The default layer for players is localPlayer, 11
+            SetLayerRecursively(gameObject, 12);
+        }
+        //If we ARE the local player...
+        else
+        {
+            //Disable the Scene Camera so the localPlayer's camera takes priority
+            SceneCamera = Camera.main;
+            if (SceneCamera != null)
+            {
+                Camera.main.gameObject.SetActive(false);
+            }
+        }
+
+        //Append the name of the Player with the Network ID, Regardless of being Local
+        gameObject.name = "Player" + netId;
+        
+        //Register players into the manager
+        /*Player player = GetComponent<Player>();
+        string netID = GetComponent<NetworkIdentity>().netId.ToString();
+        GameManager.RegisterPlayer(netID, player);*/
+    }
+
+    //Called when this object is destroyed, should only occur if the serevr Destroyed it, this should mean the localPlayer disconnected
+    public void OnDisable()
+    {
+        //Renables Scene Camera
+        if (SceneCamera != null)
+        {
+            SceneCamera.gameObject.SetActive(true);
+        }
+
+        //Unregister the player from the manager
+        //GameManager.UnregisterPlayer(gameObject.name);
+    }
+
+    //Common function to set layers recursivley
+    void SetLayerRecursively(GameObject obj, int newLayer)
+    {
+        if (null == obj)
+        {
+            return;
+        }
+
+        obj.layer = newLayer;
+
+        foreach (Transform child in obj.transform)
+        {
+            if (null == child)
+            {
+                continue;
+            }
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
+    }
+}
---
+++ OldScripts/PlayerShoot.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Mirror;
+
+[RequireComponent (typeof (WeaponManager))]
+public class PlayerShoot : NetworkBehaviour
+{
+    private PlayerWeapon currentWeapon;
+
+    public Camera camera;
+    public LayerMask mask;
+    public WeaponManager WeaponManager;
+
+    void Start()
+    {
+        WeaponManager = GetComponent<WeaponManager>();
+    }
+
+    void Update()
+    {
+        currentWeapon = WeaponManager.GetCurrentWeapon();
+        if(Input.GetButtonDown("Fire1"))
+        {
+            CmdShoot();
+        }
+    }
+
+    [Command]
+    void CmdShoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, currentWeapon.range, mask))
+        {
+            if (hit.collider.tag == "Player")
+                RpcPlayerShot(hit.collider.name, currentWeapon.damage);
+        }
+    }
+
+    [ClientRpc]
+    void RpcPlayerShot(string id, int damage)
+    {
+        Debug.Log(id + " has been shot.");
+        Player player = GameManager.GetPlayer(id);
+        player.currentHealth = player.currentHealth - currentWeapon.damage;
+    }
+}
