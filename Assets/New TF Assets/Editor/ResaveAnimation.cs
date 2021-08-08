40834be0e370084b1fa8df1aefe12439662548ec
Litl Toast <joshuafhiggins@gmail.com>
2021-08-08 12:41:33 -0400

    Finally got non laggy animations through
    lots of bs tho to get this, this was also a change made long ago but now being pushed

---
+++ Assets/New TF Assets/Editor/ResaveAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ResaveAnimation : EditorWindow
+{
+    AnimationClip clip;
+
+    [MenuItem("Window/Anim Saver")]
+    public static void ShowWindow()
+	{
+        GetWindow<ResaveAnimation>("Resave Animations");
+	}
+
+    void OnGUI()
+    {
+        clip = (AnimationClip)EditorGUILayout.ObjectField(clip, typeof(AnimationClip));
+        if(GUILayout.Button("Resave!"))
+		{
+            //Debug.Log(AssetDatabase.ExtractAsset(clip, "Assets/Resaved/" + clip.name + ".anim"));
+            AssetDatabase.CopyAsset(AssetDatabase.GetAssetPath(clip), "Assets/Resaved/" + clip.name + ".anim");
+        }
+    }
+}
