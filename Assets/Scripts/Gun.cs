4f1e6726d90a97a72bc1062af6a80d1aae4ec5e1
Litl Toast <joshuafhiggins@gmail.com>
2021-12-04 14:08:37 -0500

    New Strat for making ButterEngine

--- Assets/Scripts/Gun.cs
+++
@@ -1,8 +0,0 @@
-﻿//Basic class contianer with all of the properties of a weapon
-[System.Serializable]
-public class Gun
-{
-    public string name = "Gun";
-    public int damage = 10;
-    public float range = 100f;
-}
